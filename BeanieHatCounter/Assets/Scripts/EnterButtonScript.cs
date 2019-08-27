745f24152cd74521823c38d2e6fa1abf0106969d
callibates <fcf0178@aut.ac.nz>
2019-08-27 13:36:05 +1200

    added main scene and started with storing rows needed

---
+++ BeanieHatCounter/Assets/Scripts/EnterButtonScript.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class EnterButtonScript : MonoBehaviour
+{
+	public Text enterField;
+	static int rowsNeeded;
+    // Start is called before the first frame update
+    void Start()
+    {
+		rowsNeeded = 0;
+        Button btn = this.GetComponent<Button>();
+        btn.onClick.AddListener(onClick);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void onClick()
+    {
+		GetNum();
+        SceneManager.LoadScene("SampleScene");
+    }
+
+    void GetNum()
+	{
+		string stringVersion = enterField.text.ToString();
+		rowsNeeded = int.Parse(stringVersion);
+		Debug.Log("num is" + rowsNeeded);
+	}
+}
